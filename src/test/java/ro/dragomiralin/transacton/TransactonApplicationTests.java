3163f894451797326e500785b3c1e976f3462d7c
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-06-06 19:55:15 +0300

    update setup

---
+++ src/test/java/ro/dragomiralin/transacton/TransactonApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dragomiralin.transacton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
