88bad1fb2f87a0941385eaf82131ef5c80decadd
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-08-14 19:49:00 +0300

    add security

--- src/main/java/ro/dragomiralin/ecommerce/boot/security/SecurityConfig.java
+++ src/main/java/ro/dragomiralin/ecommerce/boot/security/SecurityConfig.java
@@ -1,4 +1,32 @@
 package ro.dragomiralin.ecommerce.boot.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v2/api-docs",
+            "/webjars/**",
+            "/actuator/**"
+    };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
 }
