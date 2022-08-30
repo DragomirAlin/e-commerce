4d784b894af1e81130364977ee05cf92ffb6a29d
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-08-30 22:10:42 +0300

    add shopping cart adapter

---
+++ repository/src/main/java/ro/dragomiralin/ecommerce/repository/cart/adapter/ShoppingCartAdapter.java
@@ -0,0 +1,42 @@
+package ro.dragomiralin.ecommerce.repository.cart.adapter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ro.dragomiralin.ecommerce.domain.cart.domain.ShoppingCartDO;
+import ro.dragomiralin.ecommerce.domain.cart.port.ShoppingCartPort;
+import ro.dragomiralin.ecommerce.domain.common.page.PageDO;
+import ro.dragomiralin.ecommerce.repository.cart.repository.ShoppingCartRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShoppingCartAdapter implements ShoppingCartPort {
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    @Override
+    public long save(ShoppingCartDO shoppingCartDO) {
+        return 0;
+    }
+
+    @Override
+    public Optional<ShoppingCartDO> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean delete(long id) {
+        return false;
+    }
+
+    @Override
+    public PageDO<ShoppingCartDO> list(int page, int size) {
+        return null;
+    }
+
+    @Override
+    public List<ShoppingCartDO> list() {
+        return null;
+    }
+}
