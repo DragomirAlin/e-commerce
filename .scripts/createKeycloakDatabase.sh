0bea3e6568f03ce720db93b8abb399fa13f5130b
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-10-14 21:39:19 +0300

    update keycloak image for macos

--- .scripts/createKeycloakDatabase.sh
+++ .scripts/createKeycloakDatabase.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CONTAINER_NAME=e-commerce_db_1
+CONTAINER_NAME=e-commerce-db-1
 docker exec -it $CONTAINER_NAME psql -U postgres -d postgres \
-    -c "CREATE database postgres" \
+    -c "CREATE database keycloak" \
     -c "CREATE USER kcuser WITH ENCRYPTED PASSWORD 'kcuser1234';" \
     -c "GRANT ALL PRIVILEGES ON DATABASE keycloak TO kcuser;" \
     -c "\l" \
