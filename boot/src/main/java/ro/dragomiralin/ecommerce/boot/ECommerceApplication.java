9d9390bd3f19a84e190d9a911f33de95344d476c
Dragomir Alin <dragomirdanielalin@gmail.com>
2023-02-25 17:51:39 +0200

    add monitoring

--- boot/src/main/java/ro/dragomiralin/ecommerce/boot/ECommerceApplication.java
+++ boot/src/main/java/ro/dragomiralin/ecommerce/boot/ECommerceApplication.java
@@ -1,9 +1,11 @@
 package ro.dragomiralin.ecommerce.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -19,5 +21,4 @@ public class ECommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
     }
-
 }
